9cff6ef37b9ccf14a57aa160d55d5c0050a8293c
Oleg Sergeev <se1@rep.earth>
2021-06-21 15:09:27 +0300

    jest config coverageReporters value udpate

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,7 @@
 module.exports = {
   collectCoverage: true,
   coverageProvider: 'babel',
-  "coverageReporters": [
-    "json-summary"
-  ],
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'json-summary'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
